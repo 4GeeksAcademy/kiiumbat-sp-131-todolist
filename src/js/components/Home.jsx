5241bc616b364f12a25ce0f95fd4cb1336a71b71
Marc Caritg <k.kiumbat@gmail.com>
2026-02-25 14:08:56 +0000

    first list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+// 1. Importamos el componente y el icono específico
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>Mi Lista</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && inputValue.trim() !== "") {
+                setTodos(todos.concat([inputValue]));
+                setInputValue("");
+              }
+            }}
+            placeholder='What do you need to do?'
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}> 
+            {item}
+            {/* 2. Sustituimos el <i> por el componente FontAwesomeIcon */}
+            <FontAwesomeIcon 
+              icon={faXmark} 
+              className="delete-icon"
+              onClick={() => {
+                setTodos(
+                  todos.filter((t, currentIndex) => index !== currentIndex)
+                );
+              }} 
+            />
+          </li>
+        ))}
+      </ul>
+      {/* Corregido el div del contador */}
+      <div>{todos.length} {todos.length === 1 ? "task" : "tasks"}</div>
+    </div>
+  );
 };
 
 export default Home;
